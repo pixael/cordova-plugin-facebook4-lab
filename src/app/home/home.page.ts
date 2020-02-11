edb755f9ddffc27511a93384bc7aa288bb547676
peterpeterparker <david.dalbusco@outlook.com>
2020-02-11 12:41:25 +0100

    feat: add test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Facebook, FacebookLoginResponse} from '@ionic-native/facebook/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor(private fb: Facebook) {
+    }
+
+
+    test() {
+        this.fb.login(['public_profile', 'user_friends', 'email'])
+            .then((res: FacebookLoginResponse) => {
+                console.log('Logged into Facebook!', res);
+                alert('Login OK');
+            })
+            .catch(e => {
+                console.log('Error logging into Facebook', e);
+                alert('Login ERROR');
+            });
+    }
 
 }
